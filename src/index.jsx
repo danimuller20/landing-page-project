7b3e6cd2424464e569e0bedb047158ded3c530ba
Daniela Muller <danielamuller20@gmail.com>
2021-07-17 16:54:17 -0300

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Home from './templates/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
